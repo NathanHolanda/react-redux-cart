e749f5cefed174b54abbeb37c9ee8f92da32355c
hl_nathan <50773888+NathanHolanda@users.noreply.github.com>
2022-07-04 14:38:41 -0300

    chore: changing back the tab width to 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,45 +5,45 @@ import useAppSelector from './hooks/useAppSelector';
 import logo from './logo.svg';
 
 function App() {
-    const counter = useAppSelector((state) => state.counter);
-    const dispatch = useAppDispatch();
+  const counter = useAppSelector((state) => state.counter);
+  const dispatch = useAppDispatch();
 
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>Hello Vite + React!</p>
-                <p>
-                    <button type="button" onClick={() => dispatch(increment())}>
-                        <span>count is: {counter.value}</span>
-                    </button>
-                </p>
-                <p>
-                    Edit <code>App.tsx</code> and save to test HMR updates.
-                </p>
-                <p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Learn React
-                    </a>
-                    {' | '}
-                    <a
-                        className="App-link"
-                        href="https://vitejs.dev/guide/features.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Vite Docs
-                    </a>
-                </p>
-            </header>
-            <br />
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => dispatch(increment())}>
+            <span>count is: {counter.value}</span>
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+      <br />
+    </div>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -6,9 +6,9 @@ import './index.css';
 import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <ReduxProvider store={store}>
-            <App />
-        </ReduxProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <ReduxProvider store={store}>
+      <App />
+    </ReduxProvider>
+  </React.StrictMode>
 );
